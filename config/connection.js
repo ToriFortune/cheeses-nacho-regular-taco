323ff9e4fba88794465ffaa27b3aa4416348d1d0
Tori Fortune <toriolley@gmail.com>
2019-06-05 21:26:14 -0500

    updated connection.js file to include jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 //  mySQL connection info
 const mysql = require ("mysql");
-
-const connection = mysql.createConnection({
-host:"localhost",
-user:"root",
-password:process.env.SQL_PASSWORD,
-database:"tacosDB"});
-
+const connection;
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    connection = mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        password:process.env.SQL_PASSWORD,
+        database:"tacosDB"
+    });
+};
 // export
+
 module.exports = connection;
